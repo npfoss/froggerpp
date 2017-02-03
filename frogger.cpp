9b4c13588ef37420a0964baa912d510d4d244972
Katie Bacher <Katie@Katies-MacBook-Pro.local>
2017-02-02 23:07:19 -0500

    background edits

--- frogger.cpp
+++ frogger.cpp
@@ -10,7 +10,7 @@
 8--------cars------------
 9--------grass-----------
 */
-#include <iostream>
+//#include <iostream>
 #include <GLUT/glut.h>
 #include <GLUT/glut.h>
 #include <vector>
@@ -66,12 +66,11 @@ float h = 2.0/SCREEN_HEIGHT;
 float c = 1.0/255;
 bool up_pressed, down_pressed, left_pressed, right_pressed = false;
 
-
 void resetFrog(){
   // TODO: start frog at the bottom in the middle
-  frog=Rect(SCREEN_WIDTH/2,SCREEN_HEIGHT - 50,0,0*c,255*c,0*c,3,3);
+  frog.y = SCREEN_HEIGHT - 5 - RECT_HEIGHT;
+  frog.x = SCREEN_HEIGHT / 2 - RECT_HEIGHT / 2;
 }
-
 
 void init()
 {
@@ -107,12 +106,6 @@ void init()
   logs.push_back(LogGoal6);
 
   resetFrog();
-}
-
-void resetFrog(){
-  // TODO: start frog at the bottom in the middle
-  frog.y = SCREEN_HEIGHT - 5 - RECT_HEIGHT;
-  frog.x = SCREEN_HEIGHT / 2 - RECT_HEIGHT / 2;
 }
 
 bool frogOnLogs(){
@@ -152,30 +145,30 @@ void idlefunc() {
     }
 
     // TODO: check if frog made it across
-    if (collidesWith(frog,logs(4)))
-    {
-        logs(4).r=0;
-        logs(4).g=255;
-        logs(4).b=0;
-        frog.x=SCREEN_WIDTH/2;
-        frog.y=SCREEN_HEIGHT*1/10;
-    }
-    if (collidesWith(frog,logs(5)))
-    {
-        logs(5).r=0;
-        logs(5).g=255;
-        logs(5).b=0;
-        frog.x=SCREEN_WIDTH/2;
-        frog.y=SCREEN_HEIGHT*1/10;
-    }
-    if (collidesWith(frog,logs(6)))
-    {
-        logs(6).r=0;
-        logs(6).g=255;
-        logs(6).b=0;
-        frog.x=SCREEN_WIDTH/2;
-        frog.y=SCREEN_HEIGHT*1/10;
-    }
+    // if (collidesWith(frog,logs[4]))
+    // {
+    //     logs[4].r=0;
+    //     logs[4].g=255;
+    //     logs[4].b=0;
+    //     frog.x=SCREEN_WIDTH/2;
+    //     frog.y=SCREEN_HEIGHT*1/10;
+    // }
+    // if (collidesWith(frog,logs[5]))
+    // {
+    //     logs[5].r=0;
+    //     logs[5].g=255;
+    //     logs[5].b=0;
+    //     frog.x=SCREEN_WIDTH/2;
+    //     frog.y=SCREEN_HEIGHT*1/10;
+    // }
+    // if (collidesWith(frog,logs[6]))
+    // {
+    //     logs[6].r=0;
+    //     logs[6].g=255;
+    //     logs[6].b=0;
+    //     frog.x=SCREEN_WIDTH/2;
+    //     frog.y=SCREEN_HEIGHT*1/10;
+    // }
 
     // check if frog dies
     if(frog.x > 5*LANE_HEIGHT){
@@ -202,7 +195,7 @@ void fillBackground(){
   glClear(GL_COLOR_BUFFER_BIT);
   // eventually have a road texture, water texture, etc
    // grass on top
-   glColor3f(0.0, 1.0, 0.0);
+   glColor3f(0.0, 153*c, 0.0);
    glBegin(GL_POLYGON);
      glVertex3f(-1.0, 1.0, 0.0);
      glVertex3f(-1.0, 1-LANE_HEIGHT*h, 0.0);
@@ -219,8 +212,8 @@ void fillBackground(){
      glVertex3f(1.0, 1-LANE_HEIGHT*h, 0.0);
    glEnd();
 
-   //middle water
-   glColor3f(0.0, 1.0, 0.0);
+   //middle grass
+   glColor3f(0.0, 153*c, 0.0);
    glBegin(GL_POLYGON);
      glVertex3f(-1.0, 1-5*LANE_HEIGHT*h, 0.0);
      glVertex3f(-1.0, 1-6*LANE_HEIGHT*h, 0.0);
@@ -252,7 +245,7 @@ void fillBackground(){
    glEnd();
 
    //grass on bottom
-   glColor3f(0.0, 1.0, 0.0);
+   glColor3f(0.0, 153*c, 0.0);
    glBegin(GL_POLYGON);
      glVertex3f(-1.0, -1.0, 0.0);
      glVertex3f(-1.0, -1+LANE_HEIGHT*h, 0.0);

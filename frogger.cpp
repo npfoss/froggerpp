9c6349431196bdd8e357812935646f1150d260f0
Nate Foss <npfoss@gmail.com>
2017-02-02 21:07:12 -0500

    finish framework, add some movement, add lane sizing (but haven't implemented

--- frogger.cpp
+++ frogger.cpp
@@ -1,3 +1,16 @@
+/* Game map by row:
+0----grass and finish----
+1--------logs------------
+2--------logs------------
+3--------logs------------
+4--------logs------------
+5--------grass-----------
+6--------cars------------
+7--------cars------------
+8--------cars------------
+9--------grass-----------
+*/
+
 #include <GLUT/glut.h>
 #include <GLUT/glut.h>
 #include <vector>
@@ -5,15 +18,22 @@ using namespace std;
 
 #define SCREEN_WIDTH 500
 #define SCREEN_HEIGHT 400
+#define LANE_HEIGHT 40
+#define RECT_HEIGHT 30
+#define JUMP_DIST 5
 #define DELAY 100000;
 
 class Rect
 {
 public:
+  // x, y of top left corner
   int x;
   int y;
+  // for movement, not sizing
   int dx;
+
   float r, g, b;
+
   int width;
   int height;
 
@@ -32,8 +52,7 @@ public:
   }
 
   bool collidesWith(Rect rect){
-
-    return false;
+    return (this->x + this->width < rect.x || this->y + this->height < rect.y || rect.x + rect.width < this->x || rect.y + rect.height < this->y)
   }
 };
 
@@ -46,29 +65,44 @@ float red = 0;
 float w = 1.0/SCREEN_WIDTH;
 float h = 1.0/SCREEN_HEIGHT;
 float c = 1.0/<p></p>255;
-
+bool up_pressed, down_pressed, left_pressed, right_pressed = false;
 
 void init()
 {
-   Rect Car0=Rect(SCREEN_WIDTH/4,SCREEN_HEIGHT/4,5,244*c,208*c,63*c,4,4);
-   Rect Car1=Rect(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,8,155*c,89*c,182*c,4,4);
+  // for row r of cars/logs
+    // x = various
+    // y = r * LANE_HEIGHT + (LANE_HEIGHT - RECT_HEIGHT) / 2
 
-   cars.push_back(Car0);
-   cars.push_back(Car1);
 
-   Rect Log0=Rect(SCREEN_WIDTH/2,SCREEN_HEIGHT/4,5,255*c,248*c,220*c,10,3);
-   Rect Log1=Rect(SCREEN_WIDTH*3/4,SCREEN_HEIGHT/4,5,255*c,248*c,220*c,10,3);
-   Rect Log2=Rect(SCREEN_WIDTH/2,SCREEN_HEIGHT*3/4,5,255*c,248*c,220*c,10,3);
 
-   Rect LogGoal0=Rect(SCREEN_WIDTH,SCREEN_HEIGHT,0,255*c,255*c,255*c,4,4);
-   Rect LogGoal1=Rect(SCREEN_WIDTH,SCREEN_HEIGHT,0,255*c,255*c,255*c,4,4);
-   Rect LogGoal2=Rect(SCREEN_WIDTH,SCREEN_HEIGHT,0,255*c,255*c,255*c,4,4);
+  Rect Car0=Rect(SCREEN_WIDTH/4,SCREEN_HEIGHT/4,5,244*c,208*c,63*c,4,RECT_HEIGHT);
 
-   logs.push_back(Log0);
-   logs.push_back(Log1);
-   logs.push_back(Log2);
 
-   frog=Rect(SCREEN_WIDTH/2,SCREEN_HEIGHT - 50,0,0*c,255*c,0*c,3,3);
+
+
+  Rect Car1=Rect(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,8,155*c,89*c,182*c,4,4);
+
+  cars.push_back(Car0);
+  cars.push_back(Car1);
+
+  Rect Log0=Rect(SCREEN_WIDTH/2,SCREEN_HEIGHT/4,5,255*c,248*c,220*c,10,3);
+  Rect Log1=Rect(SCREEN_WIDTH*3/4,SCREEN_HEIGHT/4,5,255*c,248*c,220*c,10,3);
+  Rect Log2=Rect(SCREEN_WIDTH/2,SCREEN_HEIGHT*3/4,5,255*c,248*c,220*c,10,3);
+
+  Rect LogGoal0=Rect(SCREEN_WIDTH,SCREEN_HEIGHT,0,255*c,255*c,255*c,4,4);
+  Rect LogGoal1=Rect(SCREEN_WIDTH,SCREEN_HEIGHT,0,255*c,255*c,255*c,4,4);
+  Rect LogGoal2=Rect(SCREEN_WIDTH,SCREEN_HEIGHT,0,255*c,255*c,255*c,4,4);
+
+  logs.push_back(Log0);
+  logs.push_back(Log1);
+  logs.push_back(Log2);
+
+  resetFrog();
+}
+
+void resetFrog(){
+  // TODO: start frog at the bottom in the middle
+  frog=Rect(SCREEN_WIDTH/2,SCREEN_HEIGHT - 50,0,0*c,255*c,0*c,3,3);
 }
 
 void idlefunc() {
@@ -77,6 +111,7 @@ void idlefunc() {
     count = 0;
     red = red + .01;
 
+    // move everything
     for(auto car : cars){
       car.x += car.dx;
     }
@@ -85,7 +120,36 @@ void idlefunc() {
       car.x += car.dx;
     }
 
+    if (up_pressed){
+      frog.y -= JUMP_DIST;
+    }
+    if (down_pressed){
+      frog.y += JUMP_DIST;
+    }
+    if (left_pressed){
+      frog.x -= JUMP_DIST;
+    }
+    if (right_pressed){
+      frog.x += JUMP_DIST;
+    }
 
+    // TODO: check if frog made it across
+
+
+    // check for collisions
+    for(auto car : cars){
+      if(frog.collidesWith(car)){
+        // ded
+        resetFrog();
+      }
+    }
+
+    for(auto car : logs){
+      if(frog.collidesWith(car)){
+        // ded
+        resetFrog();
+      }
+    }
 
     glutPostRedisplay();
   }
